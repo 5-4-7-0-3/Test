e66147394ee879ded1c4921bea55aa0cb0d0bb0b
Freak <39245775+KepKepKep@users.noreply.github.com>
2022-11-07 15:39:24 +0200

    added and configured db

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { setupDb } from "./db/setup-db";
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+setupDb();
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
