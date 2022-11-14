f791cca8b6a077cc1b665fc09883b04f22d6e3a6
Freak <39245775+KepKepKep@users.noreply.github.com>
2022-11-14 14:06:42 +0200

    fix cors headers

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,15 @@ const PORT = process.env.PORT || 8000;
 const app = require("express")();
 const server = require("http").createServer(app);
 
+// Add headers before the routes are defined
+// app.use(function (req, res, next) {
+//     res.set({
+//         // "Content-Type": "application/json",
+//         // "Access-Control-Allow-Origin": "http://127.0.0.1:3000"
+//     });
+//     next();
+// });
+
 app.use(express.json());
 app.use("/", userRout);
 
